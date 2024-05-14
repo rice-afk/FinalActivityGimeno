9b112493993532f839d0a512ceb5430c29b521fb
rice-afk <82850664+rice-afk@users.noreply.github.com>
2024-05-14 17:50:03 +0800

    Final Activity

--- src/main/java/FinalActivity/Methods/AjaxFormDemoMethods.java
+++ src/main/java/FinalActivity/Methods/AjaxFormDemoMethods.java
@@ -2,41 +2,63 @@ package FinalActivity.Methods;
 
 import FinalActivity.demoPage.AjaxFormSubmitDemoPage;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.time.Duration;
 
 public class AjaxFormDemoMethods {
     private WebDriver driver;
     String url = "https://demo.seleniumeasy.com/ajax-form-submit-demo.html";
 
-    public void testAjaxFormSubmit() {
-        setUp();
-        driver.get(url);
-
-        AjaxFormSubmitDemoPage page = new AjaxFormSubmitDemoPage(driver);
-        page.fillForm("John Doe", "This is a test comment.");
-        page.submitForm();
-
-        page.waitForSuccessMessageVisibiity(driver);
-        String initialMessage = page.printInitialMessage(driver);
-        try {
-            Assert.assertEquals("Ajax Request is Processing!", initialMessage, "Initial message does not match.");
-        } catch (AssertionError e) {
-            tearDown();
-            throw e; // Re-throw the exception to fail the test
+    public JSONArray loadJsonData(String filePath) throws IOException {
+        File file = new File(filePath);
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while ((line = br.readLine())!= null) {
+            sb.append(line);
         }
-        page.waitForSuccessMessage(driver);
-        String successMessage = page.printFinalMessage();
-        try {
-            Assert.assertEquals("Form submitted Successfully!", successMessage, "Success message does not match.");
-        } catch (AssertionError e) {
+        br.close();
+        return new JSONArray(sb.toString());
+    }
+
+    public void testAjaxFormSubmit() throws IOException {
+        JSONArray inputData = loadJsonData("/Users/louisgimeno/IdeaProjects/MavenDependencies/AjaxSubmitForm.json");
+        for (int i = 0; i < inputData.length(); i++) {
+            setUp();
+            driver.get(url);
+
+            AjaxFormSubmitDemoPage page = new AjaxFormSubmitDemoPage(driver);
+            JSONObject data = inputData.getJSONObject(i);
+            page.fillForm(data.getString("name"), data.getString("comment"));
+            page.submitForm();
+
+            page.waitForSuccessMessageVisibiity(driver);
+            String initialMessage = page.printInitialMessage(driver);
+            try {
+                Assert.assertEquals("Ajax Request is Processing!", initialMessage, "Initial message does not match.");
+            } catch (AssertionError e) {
+                tearDown();
+                throw e; // Re-throw the exception to fail the test
+            }
+            page.waitForSuccessMessage(driver);
+            String successMessage = page.printFinalMessage();
+            try {
+                Assert.assertEquals("Form submitted Successfully!", successMessage, "Success message does not match.");
+            } catch (AssertionError e) {
+                tearDown();
+                throw e; // Re-throw the exception to fail the test
+            }
             tearDown();
-            throw e; // Re-throw the exception to fail the test
         }
-        tearDown();
     }
 
     public void setUp() {
--- src/main/java/FinalActivity/tests/AjaxFormSubmitTests.java
+++ src/main/java/FinalActivity/tests/AjaxFormSubmitTests.java
@@ -5,11 +5,13 @@ import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
+import java.io.IOException;
+
 public class AjaxFormSubmitTests {
     private WebDriver driver;
 
     @Test
-    public void testAjaxFormSubmit() {
+    public void testAjaxFormSubmit() throws IOException {
         AjaxFormDemoMethods page = new AjaxFormDemoMethods();
         page.testAjaxFormSubmit();
     }
